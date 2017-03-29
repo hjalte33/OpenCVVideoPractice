28ee3fe77a0506f48855a3abc9fcda7f6b015d21
Hjalte Nielsen <hjalte33@gmail.com>
2017-03-29 19:11:17 +0200

    bug fixes

--- OpenCVVideoPractice/OpenCVVideoPractice.cpp
+++ OpenCVVideoPractice/OpenCVVideoPractice.cpp
@@ -30,23 +30,23 @@ int main(int argc, char** argv[])
 	cvtColor(prevFrame, prevFrame, CV_RGB2GRAY, 1);
 	
 	while (true) {
-		if (!video.read(frame))
+		if (!video.read(frame)) {
 			break;
-		video >> frame;
-		cvtColor(frame, frame, CV_RGB2GRAY, 1);
+		}
 
+		cvtColor(frame, frame, CV_RGB2GRAY, 1);
 		absdiff(frame, prevFrame, diff);
 		threshold(diff, binary, 40, 255, THRESH_BINARY);
 
 		imshow("Frame", frame);
 		imshow("Threshold", binary);
 
-		int offset = 10;
+		int offset = 1;
 		video.set(CV_CAP_PROP_POS_FRAMES, video.get(CV_CAP_PROP_POS_FRAMES) - offset);
-		
+
 		video >> prevFrame;
 		cvtColor(prevFrame, prevFrame, CV_RGB2GRAY, 1);
-		video.set(CV_CAP_PROP_POS_FRAMES, video.get(CV_CAP_PROP_POS_FRAMES) + offset - 1);
+		video.set(CV_CAP_PROP_POS_FRAMES, video.get(CV_CAP_PROP_POS_FRAMES) + offset -1);
 		
 		char key = cvWaitKey(10);
 		if (key == 27) // ESC
